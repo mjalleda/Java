cd979ea50463b33e7d3b3c654ac92dbd8421c05a
Mamatha Jalleda <mksmile05@gmail.com>
2021-01-18 17:35:18 -0800

    Update Exer18_SubInterface.java

--- Javaassignment/src/Assign1_fundamentalsOne/Exer18_SubInterface.java
+++ Javaassignment/src/Assign1_fundamentalsOne/Exer18_SubInterface.java
@@ -1,5 +1,9 @@
 /* Description: In this class, we see how a interface use keoword "extends" by extending  other interfaces.
-Relationship: Exer13_InterfaceA, Exer13_InterfaceB, Exer13_SubInterface are interfaces */
+Relationship: Exer13_InterfaceA, Exer13_InterfaceB, Exer13_SubInterface are interfaces 
+So basically Exer18_SubInterface inherting all methods/proeprties from Exer18_InterfaceA & Exer18_InterfaceB
+when a concrete/abstract class extends this interface(Exer18_SubInterface) - they will inherit all methods/variables from three interfaces. they
+are (Exer18_InterfaceA, Exer18_InterfaceB and Exer18_SubInterface).
+*/
 package Assign1_fundamentalsOne;
 
 public interface Exer18_SubInterface extends Exer18_InterfaceA, Exer18_InterfaceB{
